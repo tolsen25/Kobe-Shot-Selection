31507be23b97e444b40de9aa7175354b018cbc37
Thomas Olsen <t.olsen18@outlook.com>
2023-12-06 16:38:29 -0700

    .607

--- forestModel.R
+++ forestModel.R
@@ -12,7 +12,7 @@ data2 = data %>% mutate(
   shot_made_flag = as.factor(shot_made_flag),
   finalMinute = ifelse(minutes_remaining == 0, 1,0),
   trueBeater = ifelse(minutes_remaining == 0, ifelse(seconds_remaining <= 3,1,0),0),
-  endOfQuarterHeave = if_else(period < 4 & shot_distance > 20 & seconds_remaining < 5,1,0)
+
   # period = as.factor(period),
   #season_team = str_c(season, opponent, sep = "_")
   
@@ -20,33 +20,43 @@ data2 = data %>% mutate(
   
 ) %>% select(c(shot_made_flag, shot_distance, shot_id, period, 
                seconds_remaining, action_type, opponent, minutes_remaining,loc_x,loc_y,
-               opponent,season, shot_zone_area, playoffs))
+               opponent,season, shot_zone_area, playoffs, finalMinute,trueBeater))
 
 
 train = data2 %>% filter(!is.na(shot_made_flag))
 test = data2 %>% filter(is.na(shot_made_flag))
 
-treeModel = rand_forest(mtry = tune(), min_n =  tune(), trees = 500) %>%
+treeModel = rand_forest(mtry = 2, min_n =  40, trees = 500) %>%
   set_engine("ranger") %>%
   set_mode("classification")
 
 my_recipe <- recipe(shot_made_flag ~ ., data=train) %>%
-  step_mutate_at(all_numeric_predictors(), fn = factor)  %>% # turn all numeric features into factors5
-  # step_other(all_nominal_predictors(), threshold = .001) %>% 
-  step_lencode_mixed(all_nominal_predictors(), outcome = vars(shot_made_flag)) 
+  #step_mutate_at(all_numeric_predictors(), fn = factor)  %>% # turn all numeric features into factors5
+  #step_other(all_nominal_predictors(), threshold = .01) %>% 
+  #step_date(game_date) %>% 
+  step_zv(all_predictors()) %>% 
+  step_rm(shot_id) %>% 
+  #step_corr(all_numeric_predictors(), threshold = .7) %>% 
+  step_normalize(all_numeric_predictors()) %>% 
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(shot_made_flag))  
+#step_smote(all_outcomes(), neighbors = 5)
 
+
+prepped = prep(my_recipe)
+# 
+x = bake(prepped, new_data = train)
 
 forestReg_workflow = workflow()  %>%
   add_recipe(my_recipe) %>% add_model(treeModel)
-
-tuning_grid = grid_regular(mtry(range = c(1,5)), min_n(), levels = 5)# idk what this does
-
-folds = vfold_cv(train, v = 5, repeats = 1)
-
-CV_results = forestReg_workflow %>% tune_grid(resamples = folds, grid = tuning_grid,
-                                              metrics = metric_set(mn_log_loss))
-
-bestTune = CV_results %>% select_best("mn_log_loss")
+# 
+# tuning_grid = grid_regular(mtry(range = c(1,5)), min_n(), levels = 5)# idk what this does
+# 
+# folds = vfold_cv(train, v = 5, repeats = 1)
+# 
+# CV_results = forestReg_workflow %>% tune_grid(resamples = folds, grid = tuning_grid,
+#                                               metrics = metric_set(mn_log_loss))
+# 
+# bestTune = CV_results %>% select_best("mn_log_loss")
 
 final_wf = forestReg_workflow %>% finalize_workflow(bestTune) %>% fit(train)
 
@@ -59,8 +69,9 @@ sub = test %>% mutate(
   
 ) %>% select(shot_id, shot_made_flag)
 
+print(summary(sub$shot_made_flag))
 
-vroom_write(sub, "kobeRandomForest.csv", delim = ",")
+vroom_write(sub, "kobeRandomForest3.csv", delim = ",")
 
 
 
